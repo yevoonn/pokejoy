540fcf8bb8cc253b6fb1eaa516ecf960fb8f3270
Przemysław Żabkiewicz <yevoonn@gmail.com>
2026-05-24 01:17:43 +0200

    Added Apollo Client. Fetching data from Pokeapi using GraphQL query.

--- src/App.jsx
+++ src/App.jsx
@@ -1,86 +1,134 @@
-import { useState, useEffect } from 'react';
-import { CssVarsProvider } from '@mui/joy/styles';
-import { CssBaseline, Typography } from '@mui/joy';
-import Sheet from '@mui/joy/Sheet';
-import CardListComponent from './components/card/CardListComponent';
-import Button from '@mui/joy/Button';
-import CircularProgress from '@mui/joy/CircularProgress';
-import Sync from '@mui/icons-material/Sync';
-import Box from '@mui/joy/Box';
+import { useState } from "react";
+import { CssVarsProvider } from "@mui/joy/styles";
+import { CssBaseline, Typography } from "@mui/joy";
+import Sheet from "@mui/joy/Sheet";
+import CardListComponent from "./components/card/CardListComponent";
+import Button from "@mui/joy/Button";
+import CircularProgress from "@mui/joy/CircularProgress";
+import Sync from "@mui/icons-material/Sync";
+import Box from "@mui/joy/Box";
+import { gql } from "@apollo/client";
+import { useQuery } from "@apollo/client/react";
+
+const GET_POKEMON_DATA = gql`
+  query pokemonListQuery($limit: Int!, $offset: Int!) {
+    pokemon(limit: $limit, offset: $offset, order_by: { id: asc }) {
+      id
+      name
+
+      pokemontypes {
+        type {
+          name
+        }
+      }
+
+      pokemonsprites {
+        sprites
+      }
+    }
+  }
+`;
 
 function App() {
-	const [isLoading, setIsLoading] = useState(false);
-	const [pokemonData, setPokemonData] = useState({});
-	const [offset, setOffset] = useState(0);
-	const limit = 10;
+  const limit = 10;
+  const [offset, setOffset] = useState(0);
+  const { loading, error, data, fetchMore, networkStatus } = useQuery(
+    GET_POKEMON_DATA,
+    {
+      variables: {
+        limit,
+        offset: 0,
+      },
+      notifyOnNetworkStatusChange: true,
+    },
+  );
 
-	const fetchPokemonData = (currentOffset) => {
-		return fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${currentOffset}`)
-			.then(response => response.json())
-			.then(async data => {
-				const pokemonList = await Promise.all(
-					data.results.map(async (pokemon) => {
-						const response = await fetch(pokemon.url);
-						const pokemonDetails = await response.json();
+  const handleLoadMore = async () => {
+    const newOffset = offset + limit;
 
-						return {
-							id: pokemonDetails?.id,
-							name: pokemonDetails?.name,
-							types: pokemonDetails?.types.map(
-								typeInfo => typeInfo.type.name
-							),
-							image: pokemonDetails?.sprites?.other?.['official-artwork']
-								.front_default,
-						};
-					})
-				);
+    await fetchMore({
+      variables: {
+        offset: newOffset,
+        limit,
+      },
+      updateQuery: (prevResult, { fetchMoreResult }) => {
+        if (!fetchMoreResult) {
+          return prevResult;
+        }
 
-				const newData = pokemonList.reduce((acc, p) => ({ ...acc, [p.id]: p }), {});
-				setPokemonData(prev => ({ ...prev, ...newData }));
-			})
-			.catch(error => {
-				console.error('Error fetching Pokémon data:', error);
-			});
-	};
+        return {
+          pokemon: [...prevResult.pokemon, ...fetchMoreResult.pokemon],
+        };
+      },
+    });
 
-	useEffect(() => {
-		fetchPokemonData(0);
-	}, []);
+    setOffset(newOffset);
+  };
 
-	const handleLoadMore = () => {
-		setIsLoading(true);
-		const newOffset = offset + limit;
-		setOffset(newOffset);
-		fetchPokemonData(newOffset).finally(() => setIsLoading(false));
-	};
+  if (networkStatus === 1) {
+    return (
+      <Typography level="h1" sx={{ mb: 4, textAlign: "center" }}>
+        Loading...
+      </Typography>
+    );
+  }
 
-	return !Object.keys(pokemonData).length ?
-		<Typography level="h1" sx={{ mb: 4, textAlign: "center" }}>
-			Loading...
-		</Typography>
-		:
-		(
-			<CssVarsProvider>
-				<CssBaseline />
-				<Sheet sx={{ minHeight: "100vh", p: 4, bgcolor: 'background.level1' }}>
-					<Typography level="h1" sx={{ mb: 4, textAlign: "center" }}>
-						PokeJoy
-					</Typography>
-					<CardListComponent cards={Object.values(pokemonData)} />
-					<Box sx={{ display: 'flex', justifyContent: 'center', mt: 4 }}>
-						<Button 
-							onClick={handleLoadMore}
-							startDecorator={isLoading ? <CircularProgress size="sm" /> : <Sync />}
-							sx={{
-								"--Button-gap": "8px"
-							}}
-						>
-							{isLoading ? 'Loading...' : 'Load more'}
-						</Button>
-					</Box>
-				</Sheet>
-			</CssVarsProvider>
-		)
+  if (error) {
+    return <Typography level="h1">Error loading data</Typography>;
+  }
+
+  const pokemonData = data.pokemon.map((pokemon) => ({
+    id: pokemon.id,
+    name: pokemon.name,
+    types: pokemon.pokemontypes.map((typeInfo) => typeInfo.type.name),
+    image:
+      pokemon.pokemonsprites?.[0]?.sprites?.other?.["official-artwork"]
+        ?.front_default,
+  }));
+
+  return (
+    <CssVarsProvider>
+      <CssBaseline />
+
+      <Sheet
+        sx={{
+          minHeight: "100vh",
+          p: 4,
+          bgcolor: "background.level1",
+        }}
+      >
+        <Typography
+          level="h1"
+          sx={{
+            mb: 4,
+            textAlign: "center",
+          }}
+        >
+          PokeJoy
+        </Typography>
+
+        <CardListComponent cards={pokemonData} />
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            mt: 4,
+          }}
+        >
+          <Button
+            onClick={handleLoadMore}
+            startDecorator={loading ? <CircularProgress size="sm" /> : <Sync />}
+            sx={{
+              "--Button-gap": "8px",
+            }}
+          >
+            Load more
+          </Button>
+        </Box>
+      </Sheet>
+    </CssVarsProvider>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-import '@fontsource/inter';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+import App from "./App.jsx";
+import "@fontsource/inter";
 
-createRoot(document.getElementById('root')).render(
+const client = new ApolloClient({
+  link: new HttpLink({ uri: "https://graphql.pokeapi.co/v1beta2" }),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </StrictMode>,
-)
+);
